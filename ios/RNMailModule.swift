ab92ee26ec2911bcd9ba1f607451d30e52258fd9
TheKeeroll <57570053+TheKeeroll@users.noreply.github.com>
2022-07-27 13:53:53 +0300

    tests

--- ios/RNMailModule.swift
+++ ios/RNMailModule.swift
@@ -7,8 +7,49 @@
 
 import Foundation
 
+
+
 @objc(RNMailModule)
 class RNMailModule: NSObject {
+  @objc var mIMAPSession;
+  @objc var mSMTPSession;
+
+  @objc var mParams : NSDictionary;
+
+  @objc init(){
+    mIMAPSession = MCOIMAPSession();
+    mSMTPSession = MCOSMPTSession();
+  }
+
+  @objc
+  func SetParams(_ params: NSDictionary) -> void {
+    mParams = params;
+  }
+
+  @objc func Login(_ resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) -> void {
+    mIMAPSession.hostname = mParams["hostname"];
+    mIMAPSession.port = mParams["port"];
+    mIMAPSession.username = mParams["username"];
+    mIMAPSession.password = mParams["passowrd"];
+    mIMAPSession.connectionType = mParams["connectionType"];
+
+    mSMTPession.hostname = mParams["hostname"];
+    mSMTPession.port = mParams["port"];
+    mSMTPession.username = mParams["username"];
+    mSMTPession.password = mParams["passowrd"];
+    mSMTPession.connectionType = mParams["connectionType"];
+    
+    if let connectOp = mIMAPSession.connectOperation(){
+      connectOp.start{ error in
+        resolve(error);
+      }
+    } else {
+        reject("FAIL");
+    }
+
+  }
+
+
   @objc
   func constantsToExport() -> [AnyHashable : Any]! {
     return ["count": 1]
